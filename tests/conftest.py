977eaa7284b8e2b5898647858f6cccc63a0e68ff
leo <leo@riskledger.com>
2023-02-28 10:49:12 +0000

    test: add portfolio definition fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,11 @@ def prices():
 @pytest.fixture()
 def currency():
     return "tests/test_files/test_currency.csv"
+
+
+@pytest.fixture()
+def portfolio_definition():
+    portfolio_definition = {
+        "TEST": {"weight": 1, "currency": "usd"},
+    }
+    return portfolio_definition
--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -1,25 +1,25 @@
 from invest_tools.portfolio import Currency, Portfolio
 
 
-def test_portfolio_ping():
+def test_portfolio_ping(portfolio_definition):
     """
     GIVEN
     WHEN Portfolio.ping is called
     THEN ping is returned
     """
     cur = Currency.GBP
-    ping = Portfolio(cur).ping()
+    ping = Portfolio(portfolio_definition, cur).ping()
     assert ping == "pong"
 
 
-def test_portfolio_calculate_returns(prices, currency):
+def test_portfolio_calculate_returns(portfolio_definition, prices, currency):
     """
     GIVEN a csv of prices and currency
     WHEN portfolio.calculate_returns is called
     THEN a DataFrame is returned
     """
     cur = Currency.GBP
-    port = Portfolio(cur)
+    port = Portfolio(portfolio_definition, cur)
     prices_df = port.get_prices(prices)
     test_code = "TEST"
     port.get_usd_converter(currency)
@@ -29,39 +29,36 @@ def test_portfolio_calculate_returns(prices, currency):
     assert len(returns) > 0
 
 
-def test_portfolio_get_usd_converter(currency):
+def test_portfolio_get_usd_converter(portfolio_definition, currency):
     """
     GIVEN a csv of currency
     WHEN portfolio.get_usd_converter is called
     THEN a class attribute is defined
     """
     cur = Currency.GBP
-    port = Portfolio(cur)
+    port = Portfolio(portfolio_definition, cur)
     port.get_usd_converter(currency)
     assert len(port.gbpusd) > 0
 
 
-def test_portfolio_get_prices(prices):
+def test_portfolio_get_prices(portfolio_definition, prices):
     """
     GIVEN a csv of prices
     WHEN portfolio.get_prices is called
     THEN a class attribute is defined
     """
     cur = Currency.GBP
-    port = Portfolio(cur)
+    port = Portfolio(portfolio_definition, cur)
     port.get_prices(prices)
     assert len(port.prices) > 0
 
 
-def test_portfolio_build(currency, prices):
+def test_portfolio_build(portfolio_definition, currency, prices):
     """
     GIVEN a portfolio definition
     WHEN portfolio.build is called
     THEN a backtest attribute is created and calculated
     """
-    portfolio_definition = {
-        "TEST": {"weight": 1, "currency": "usd"},
-    }
     cur = Currency.GBP
     port = Portfolio(portfolio_definition, cur)
     port.get_usd_converter(currency)
